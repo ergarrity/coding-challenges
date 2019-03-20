fb74537ed3a0905a34db74e82040e4be06439ca4
Liz Garrity <ergarrity@gmail.com>
2019-03-20 10:48:16 -0700

    Created file for basic linked list algorithms and implemented Node and LinkedList classes

---
+++ data-structures/linked-lists/llalgorithms.js
@@ -0,0 +1,14 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+}
+

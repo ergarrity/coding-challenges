57bb132afb526435ef9b8445a62ee48552bf156b
Liz Garrity <ergarrity@gmail.com>
2019-03-20 10:56:58 -0700

    Implemented insert method within LinkedList class that creates a new node for an input value and adds that node to the end of the linked list

--- data-structures/linked-lists/llalgorithms.js
+++ data-structures/linked-lists/llalgorithms.js
@@ -10,5 +10,18 @@ class LinkedList{
         this.head = null;
         this.tail = null;
     }
+
+    // insert @ end of list
+    insert(value){
+        let node = new Node(value);
+
+        if (!this.head){
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+    }
 }
 

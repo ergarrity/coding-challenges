7aef1d318bacd8ca0d95f5faeedb58f5de2803ff
Liz Garrity <ergarrity@gmail.com>
2019-03-22 14:06:02 -0700

    Implemented MinHeap prototype with insert and remove methods

---
+++ data-structures/heaps/heaps.js
@@ -0,0 +1,56 @@
+let MinHeap = function() {
+    this.heap = [];
+}
+
+MinHeap.prototype.insert = function(value){
+    this.heap[this.heap.length] = value;
+    this.minHeapify();
+}
+
+MinHeap.prototype.minHeapify = function(){
+    let i = this.heap.length - 1;
+
+    while (i > 0 && this.heap[i] < this.heap[Math.floor((i - 1)/2)]){
+        let temp = this.heap[i];
+        this.heap[i] = this.heap[Math.floor((i-1)/2)];
+        this.heap[Math.floor((i-1)/2)] = temp;
+        i = Math.floor((i - 1) / 2);
+    }
+}
+
+MinHeap.prototype.remove = function(value){
+    if (this.heap.length === 0){
+        return false;
+    }
+    
+    let i = 0;
+    while (this.heap[i] !== value){
+        if (i === this.heap.length - 1){
+            return false;
+        }
+        i++;
+    }
+    if (i === this.heap.length - 1) {
+        this.heap.pop();
+        return true;
+    }
+
+    this.heap[i] = this.heap.pop();
+    let left = i*2 + 1;
+    let right = i*2 + 2;
+
+    while (left < this.heap.length && this.heap[i] > this.heap[left]
+            || this.heap[i] > this.heap[right]){
+            
+                if (this.heap[left] < this.heap[right]){
+                    let temp = this.heap[i];
+                    this.heap[i] = this.heap[left];
+                    this.heap[left] = temp;
+                } else {
+                    let temp = this.heap[i];
+                    this.heap[i] = this.heap[right];
+                    this.heap[right] = temp;
+                }
+    }
+    return true;
+}
